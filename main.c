a9ac59e5129b845fd1032352776a6d31ad314c9f
houssem hammami <40180031+houssem121@users.noreply.github.com>
2024-06-05 11:51:59 -0400

    fork()ing

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sysexits.h>
 #define MAX_LINE 80 /* The maximum length command */
 
 char **parseInput(char input[], int *numWords)
@@ -12,25 +14,28 @@ char **parseInput(char input[], int *numWords)
     int count = 0;
     while (strToken != NULL)
     {
-        wordArray = (char **)realloc(wordArray, (count + 1) * sizeof(char *));//size of (pointer to char )
-        wordArray[count] = strdup(strToken); // Copy the token and then gives the pointer to be stored in the array of pointer to pointer to char **
-        count++;//inc number of tokens 
-        printf("%s\n", strToken);
-        // On demande le token suivant.
+        wordArray = (char **)realloc(wordArray, (count + 1) * sizeof(char *)); // size of (pointer to char )
+        wordArray[count] = strdup(strToken);                                   // Copy the token and then gives the pointer to be stored in the array of pointer to pointer to char **
+        count++;                                                               // inc number of tokens
+        // printf("%s\n", strToken);
+        //  On demande le token suivant.
         strToken = strtok(NULL, separators);
     }
     *numWords = count;
+
     return wordArray;
 };
 
-int main(int argc, char *argv[])
+int main(void)
 {
-
+    char *history[MAX_LINE * 10];
+    int record = 0;
     int should_run = 1; /* flag to determine when to exit program */
     while (should_run)
     {
         int numTokens;
         char input[MAX_LINE];
+
         printf("osh>");
         fflush(stdout); // Ensure "osh>" is displayed immediately
         if (fgets(input, MAX_LINE, stdin) == NULL)
@@ -39,14 +44,51 @@ int main(int argc, char *argv[])
         }
 
         char **words = parseInput(input, &numTokens);
-
+        char *args[numTokens + 1];
         // Print the words
         for (int i = 0; i < numTokens; i++)
-        {   
+        {
             printf("Word %d: %s\n", i + 1, words[i]);
-            free(words[i]); // Free memory for each word
+            history[record + i] = words[i];
+            args[i] = words[i];
         }
+        // history saving
+        int j = 0;
+        record = record + numTokens;
+        do
+        {
+            printf("%s:\n", history[j]);
+            j++;
+        } while (history[j] != NULL);
+        // exit maybe later i will do it
+    
+        if (fork() == 0)
+        { // child
+            char *command = args[0];
+            args[numTokens + 1] = NULL;
+            printf("Executing the command\n");
+            int status_code = execvp(command, args);
 
+            if (status_code == -1)
+            {
+                printf("Process did not terminate correctly\n");
+                exit(1);
+            }
+
+            else
+            {
+                printf("This line will not be printed if execvp() runs correctly\n");
+            }
+        } //
+        else
+        { // parent
+
+            if (strcmp(args[numTokens - 1], "&") != 10)
+            {
+
+                wait(NULL);
+            }
+        }
         /**
          * After reading user input, the steps are:
          * (1) fork a child process using fork()
