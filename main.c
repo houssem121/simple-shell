f4fe2755cc0537c4c5167eaaf603dff8e6049a49
houssem hammami <40180031+houssem121@users.noreply.github.com>
2024-06-06 12:16:56 -0400

    cmt

--- main.c
+++ main.c
@@ -262,12 +262,7 @@ int main(void)
 
             exit(0);
         }
-        /**
-         * After reading user input, the steps are:
-         * (1) fork a child process using fork()
-         * (2) the child process will invoke execvp()
-         * (3) parent will invoke wait() unless command included &
-         */
+        
     }
     return 0;
 }
